515d7c2f1094c32e7874a8e82f006ec436835fb7
Jeffrey <jc2573@cornell.edu>
2019-07-12 01:01:52 -0700

    Improve main loop

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/JChouCode/flappygo/bird"
 	"github.com/JChouCode/flappygo/bird_anim"
 	"github.com/JChouCode/flappygo/pipe"
+	"github.com/JChouCode/flappygo/scrollable"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/text"
@@ -15,19 +16,23 @@ import (
 	"time"
 )
 
+const sheight = 512.0
+const swidth = 672.0
+
 func collide(player bird.Bird, pipe pipe.Pipe) bool {
-	if player.GetBody().Min.X >= pipe.GetBody().Min.X &&
-		player.GetBody().Max.X <= pipe.GetBody().Max.X {
-		fmt.Println("collide x")
-	}
-	if player.GetBody().Min.Y >= pipe.GetBody().Min.Y &&
-		player.GetBody().Max.Y <= pipe.GetBody().Max.Y {
-		fmt.Println("collide y")
-	}
-	return 500 >= pipe.GetBody().Min.X &&
-		500 <= pipe.GetBody().Max.X &&
-		player.GetBody().Min.Y >= pipe.GetBody().Min.Y &&
-		player.GetBody().Max.Y <= pipe.GetBody().Max.Y
+	// if player.GetBody().Min.X >= pipe.GetBody().Min.X &&
+	// 	player.GetBody().Max.X <= pipe.GetBody().Max.X {
+	// 	fmt.Println("collide x")
+	// }
+	// if player.GetBody().Min.Y >= pipe.GetBody().Min.Y &&
+	// 	player.GetBody().Max.Y <= pipe.GetBody().Max.Y {
+	// 	fmt.Println("collide y")
+	// }
+	return swidth*0.4+player.GetBody().W() >= pipe.GetBody().Min.X &&
+		swidth*0.4+player.GetBody().W() <= pipe.GetBody().Max.X &&
+		player.GetBody().Min.Y >= pipe.GetBody().Min.Y-20 &&
+		player.GetBody().Max.Y <= pipe.GetBody().Max.Y+20
+	// return false
 }
 
 //Initialize window
@@ -41,19 +46,24 @@ func initWindow(t string, w float64, h float64) *pixelgl.Window {
 	if err != nil {
 		panic(err)
 	}
-	win.SetSmooth(true)
+	// win.SetSmooth(true)
 	return win
 }
 
 func run() {
-	win := initWindow("Flappy-GO", 1024, 768)
+	win := initWindow("Flappy-GO", 672, 512)
 
 	player := bird.New()
 	anim := bird_anim.New()
 
 	var pipes []pipe.Pipe
-	pipes = append(pipes, pipe.New(float64(rand.Intn(100)+200), false))
-	pipes = append(pipes, pipe.New(float64(rand.Intn(100)+200), true))
+	pipeBot, pipeTop := pipe.New()
+	pipes = append(pipes, pipeTop)
+	pipes = append(pipes, pipeBot)
+
+	bases := scrollable.New("base.png", 112, 336)
+	background := scrollable.New("background-day.png", 512, 288)
+
 	last := time.Now()
 	pipeLast := time.Now()
 
@@ -75,11 +85,16 @@ func run() {
 		}
 
 		if pipeDt > 1.2 {
-			pipes = append(pipes, pipe.New(float64(rand.Intn(100)+200), false))
-			pipes = append(pipes, pipe.New(float64(rand.Intn(100)+200), true))
+			pipeBot, pipeTop := pipe.New()
+			pipes = append(pipes, pipeTop)
+			pipes = append(pipes, pipeBot)
 			pipeLast = time.Now()
 		}
 
+		//Update background
+		for i := range background {
+			(&background[i]).Update(dt)
+		}
 		player.Update(dt)
 		anim.Update(player, dt)
 		// Update pipes
@@ -87,6 +102,12 @@ func run() {
 			(&pipes[i]).Update(dt)
 		}
 
+		//Update bases
+		for i := range bases {
+			(&bases[i]).Update(dt)
+		}
+
+		// Remove offscreen pipes
 		k := 0
 		for _, pipe := range pipes {
 			if !pipe.Offscreen() {
@@ -96,6 +117,16 @@ func run() {
 		}
 		pipes = pipes[:k]
 
+		//Cycle offscreen bases
+		if bases[0].GetBody().Max.X < 0 {
+			bases = scrollable.Cycle(bases)
+		}
+
+		//Cycle offscreen background
+		if background[0].GetBody().Max.X < 0 {
+			background = scrollable.Cycle(background)
+		}
+
 		for _, element := range pipes {
 			if collide(player, element) {
 				player.Reset()
@@ -103,18 +134,24 @@ func run() {
 			}
 		}
 
+		for _, b := range background {
+			b.Draw(win)
+		}
 		anim.Draw(win, player)
 		for _, pipe := range pipes {
 			pipe.Draw(win)
 		}
+		for _, b := range bases {
+			b.Draw(win)
+		}
 
 		//Debug
-		fmt.Fprintln(basicTxt, player.GetBody())
+		// fmt.Fprintln(basicTxt, player.GetBody())
 		// fmt.Println(player.GetBody())
-		fmt.Fprintln(basicTxt, len(pipes))
-		if len(pipes) > 0 {
-			fmt.Fprintln(basicTxt, pipes[0].GetBody())
-		}
+		// fmt.Fprintln(basicTxt, len(pipes))
+		// if len(pipes) > 0 {
+		// 	fmt.Fprintln(basicTxt, pipes[0].GetBody())
+		// }
 		// fmt.Println(pipes[0].GetBody())
 		// fmt.Fprintln(basicTxt, pipe.)
 
