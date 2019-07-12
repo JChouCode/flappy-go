7b945d5714aa6b6666b04f3eae0ff4d8bcdbf974
Jeffrey <jc2573@cornell.edu>
2019-07-11 18:23:39 -0700

    Add game assets

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -1,57 +1,63 @@
 package pipe
 
 import (
+	// "fmt"
 	"github.com/faiface/pixel"
-	// "image"
+	"image"
 	_ "image/png"
-	// "os"
+	"os"
 )
 
-// func loadPicture(path string) (pixel.Picture, error) {
-// 	file, err := os.Open(path)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer file.Close()
-// 	img, _, err := image.Decode(file)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return pixel.PictureDataFromImage(img), nil
-// }
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
 
-const velocity = 5
+var velocity = pixel.V(-200, 0)
+
+var sheet pixel.Picture
 
 type Pipe struct {
-	X1 float64
-	Y1 float64
-	X2 float64
-	Y2 float64
+	sprite *pixel.Sprite
+	body   pixel.Rect
+	bot    bool
 }
 
-func New(height float64, pos string) Pipe {
-	if pos == "top" {
-		return Pipe{1024, 768, 994, 768 - height}
+func init() {
+	sheet, _ = loadPicture("pipe.png")
+}
+
+func New(height float64, bot bool) Pipe {
+	if bot {
+		return Pipe{pixel.NewSprite(sheet, sheet.Bounds()), pixel.R(1024, height-sheet.Bounds().H(), 1024+sheet.Bounds().W(), height), bot}
 	}
-	if pos == "bottom" {
-		return Pipe{1024, 0, 994, height}
+	return Pipe{pixel.NewSprite(sheet, sheet.Bounds()), pixel.R(1024, 768-height, 1024+sheet.Bounds().W(), 768-height+sheet.Bounds().H()), bot}
+}
+
+func (p *Pipe) Update(dt float64) {
+	p.body = p.body.Moved(velocity.Scaled(dt))
+}
+
+func (p *Pipe) Draw(t pixel.Target) {
+	if p.bot {
+		p.sprite.Draw(t, pixel.IM.Moved(p.body.Center()))
+	} else {
+		p.sprite.Draw(t, pixel.IM.ScaledXY(pixel.ZV, pixel.V(1, -1)).Moved(p.body.Center()))
 	}
-	return Pipe{0, 0, 0, 0}
 }
 
-func (p *Pipe) Update() {
-	p.X1 -= velocity
-	p.X2 -= velocity
+func (p Pipe) Offscreen() bool {
+	return p.body.Min.X < 0
 }
 
-func (p Pipe) GetCoord1() pixel.Vec {
-	return pixel.V(p.X1, p.Y1)
-}
-
-func (p Pipe) GetCoord2() pixel.Vec {
-	return pixel.V(p.X2, p.Y2)
-}
-
-func (p Pipe) OutBounds() bool {
-	return p.X2 < 0
+func (p Pipe) GetBody() pixel.Rect {
+	return p.body
 }
